170d050a68cc335aef33506cef28bc42eac046bb
Marius <marius.c49@yahoo.com>
2025-10-01 14:37:22 +0100

    starting code

---
+++ src/App.vue
@@ -0,0 +1,6 @@
+<template>
+
+  <div id="app">
+    <RegistrationForm />
+  </div>
+</template>
---
+++ src/components/Registration/RegistrationForm.vue
@@ -0,0 +1,39 @@
+<template>
+
+<form @submit.prevent="handleSubmit">
+  <div>
+    <label for="username">Username:</label>
+    <input type="text" id="username" v-model="form.username" required />
+  </div>
+  <div>
+    <label for="email">Email:</label>
+    <input type="email" id="email" v-model="form.email" required />
+  </div>
+  <div>
+    <label for="password">Password:</label>
+    <input type="password" id="password" v-model="form.password" required />
+  </div>
+  <button type="submit">Register</button>
+  </form>
+
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+        form: {
+            username: '',
+            email: '',
+            password: ''
+        }
+        };
+    },
+    methods: {
+        handleSubmit() {
+        // Handle form submission logic here
+        console.log('Form submitted:', this.form);
+        }
+    }
+    };
+</script>
