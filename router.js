2060c79b6f893de4fe5d8b022f8df698a1e82d99
Marius Constantin <68522001+realmariusconstantin@users.noreply.github.com>
2025-10-06 20:55:23 +0100

    Home page complete, scanner & search complete (UI)

--- router.js
+++ router.js
@@ -1,16 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 // Correct file locations
-import Home from '@/view/Home.vue';
+import Home from '@/view/Home/Home.vue';
 import RegistrationForm from '@/components/Registration/RegistrationForm.vue';
 import LoginForm from '@/components/Login/Login.vue';
 
 const routes = [
-    { path: '/', redirect: '/register' },
+    { path: '/', component: Home },
     { path: '/home', component: Home },
     { path: '/register', component: RegistrationForm },
     { path: '/login', component: LoginForm },
-    { path: '/:pathMatch(.*)*', redirect: '/register' }
+    { path: '/:pathMatch(.*)*', redirect: '/' }
 ];
 
 export default createRouter({
